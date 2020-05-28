b6aa04cc8f25ac955b85f561c70fd8a75035bad1
akramsadat78 <safavimoghadam.78@gmail.comgit config --global user.name Your>
2020-05-29 01:24:37 +0430

    create ui for user

---
+++ src/user/UserComponent.js
@@ -0,0 +1,171 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import InputBase from '@material-ui/core/InputBase';
+
+
+const BootstrapInput = withStyles((theme) => ({
+    root: {
+        'label + &': {
+            marginTop: theme.spacing(3),
+        },
+    },
+    input: {
+        borderRadius: 4,
+        position: 'relative',
+        backgroundColor: theme.palette.background.paper,
+        border: '1px solid #ced4da',
+        fontSize: 16,
+        padding: '10px 26px 10px 12px',
+        transition: theme.transitions.create(['border-color', 'box-shadow']),
+        // Use the system font instead of the default Roboto font.
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+        '&:focus': {
+            borderRadius: 4,
+            borderColor: '#80bdff',
+            boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+        },
+    },
+}))(InputBase);
+
+const useStyles = makeStyles((theme) => ({
+    margin: {
+        margin: theme.spacing(1),
+    },
+}));
+
+
+/*
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));*/
+
+export default function UserComponent(props) {
+    const classes = useStyles();
+
+    if (props.field_type == 'Text') {
+        return (
+
+
+            <
+            label > { props.field_title }:
+            <
+            TextField error id = "filled"
+            variant = "filled" / >
+            <
+            /label>
+
+        );
+    } else if (props.field_type == 'Request Type') {
+        return (
+
+            <
+            label >
+
+            { props.field_title }:
+            <
+            TextField error id = "filled"
+            variant = "filled" / >
+            <
+            /label>
+        );
+    } else if (props.field_type == 'Location') {
+        if (props.field_options != null) {
+            /* return props.field_options.map(item =>{
+                 return <CustomizedSelects i={item.label}/>;
+             })*/
+
+            //     return <CustomizedSelects i={props.id}/>;
+            return ( <
+                ul >
+
+                {
+                    props.field_options.map(item => ( <
+                        li key = { item.lable } >
+                        <
+                        Select labelId = "demo-customized-select-label"
+                        id = "demo-customized-select"
+                        input = { < BootstrapInput / > } >
+
+
+                        <
+                        MenuItem value = { 10 } > { item.label } < /MenuItem>
+
+                        <
+                        /Select> <
+                        /li>
+                    ))
+                } <
+                /ul>
+            );
+        }
+        return ( <
+            label > { props.field_title }:
+            <
+            TextField error id = "filled"
+            variant = "filled" / >
+            <
+            /label>
+        );
+    } else {
+        return ( <
+            p > xxxxx < /p>
+        );
+    }
+}
+
+
+function CustomizedSelects(props) {
+    const classes = useStyles();
+    const [age, setAge] = React.useState('');
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+    return ( <
+        div >
+        <
+        FormControl className = { classes.margin } >
+        <
+        InputLabel id = "demo-customized-select-label" > Age < /InputLabel> <
+        Select labelId = "demo-customized-select-label"
+        id = "demo-customized-select"
+        value = { age }
+        onChange = { handleChange }
+        input = { < BootstrapInput / > } >
+        <
+        MenuItem value = "" >
+        <
+        em > None < /em> <
+        /MenuItem> <
+        MenuItem value = { 10 } > { props.i } < /MenuItem> <
+        MenuItem value = { 20 } > { props.i } < /MenuItem> <
+        MenuItem value = { 30 } > Thirty < /MenuItem> <
+        /Select> <
+        /FormControl>
+
+        <
+        /div>
+    );
+}
