9ef45e44618c53e77dcd94208015cb8f78f7537d
akramsadat78 <safavimoghadam.78@gmail.comgit config --global user.name Your>
2020-05-31 01:09:35 +0430

    component AxiosComponent for getting titles and details of forms from backend

--- src/axios/AxiosComponent.js
+++ src/axios/AxiosComponent.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+/* imports */
+import React from 'react';
 import axios from 'axios';
-import UserComponent from '../user/UserComponent';
-import UserCom from '../user/UserCom';
-import Title from '../user/Title';
-
+import FormsTitle from '../axios/FormsTitle';
+import FormDetails from '../axios/FormDetails';
 import {
     BrowserRouter as Router,
     Switch,
@@ -11,218 +10,66 @@ import {
     Link
 } from "react-router-dom";
 
-
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-    },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginRight: 36,
-    },
-    hide: {
-        display: 'none',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-    },
-    drawerOpen: {
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    drawerClose: {
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        overflowX: 'hidden',
-        width: theme.spacing(7) + 1,
-        [theme.breakpoints.up('sm')]: {
-            width: theme.spacing(9) + 1,
-        },
-    },
-    toolbar: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-    },
-}));
-
-function MiniDrawer() {
-    const classes = useStyles();
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
-
-    return ( <
-        div className = { classes.root } >
-        <
-        main className = { classes.content } >
-        <
-        div className = { classes.toolbar }
-        /> <
-        /main> <
-        /div>
-    );
-}
-
+/* component AxiosComponent for getting titles and details of forms from backend  */
 export default class AxiosComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            items: [],
-            url: ''
-        };
-    }
-
-    componentDidMount() {
-        axios.get("http://localhost:3001/api/forms")
-            .then(
-                (result) => {
-                    this.setState({
-                        isLoaded: true,
-                        items: result.data.items,
-                    });
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
-    }
-
-    compt() {
-        axios.get("http://localhost:3001/api/forms")
-            .then(
-                (result) => {
-                    this.setState({
-                        isLoaded: true,
-                        items: result.data.items,
-                    });
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
-    }
-
-
-    func(section) {
-        this.setState({
-            url: section,
-        });
-    }
-
-
-
-    render() {
-
-        const { error, isLoaded, items } = this.state;
-        if (error) {
-            return <div > Error: { error.message } < /div>;
-        } else if (!isLoaded) {
-            return <div > Loading... < /div>;
-        } else {
-
-
-            return ( <
-                Router >
-
-                <
-                Route path = { `/` }
-                component = {
-                    () => < Title i = { items }
-                    /> 
-                }
-                />
-
-                <
-                MiniDrawer / >
-                <
-                div >
-                <
-                nav >
-                <
-                ul > {
-                    items.map((item, index) => ( <
-                        li key = { index } >
-                        <
-                        Route path = { `/user${index + 1}` }
-                        component = {
-                            () => < UserCom i = { items }
-                            /> 
-                        }
-                        />
-
-
-                        <
-                        /li>
-                    ))
-                } <
-                /ul> <
-                /nav> <
-                /div> <
-                /Router>
-            );
+        constructor(props) {
+            super(props);
+            this.state = {
+                error: null,
+                isLoaded: false,
+                items: [],
+                url: ''
+            };
         }
-    }
-}
+
+        componentDidMount() {
+            axios.get("http://localhost:3001/api/forms")
+                .then(
+                    (result) => {
+                        this.setState({
+                            isLoaded: true,
+                            items: result.data.items,
+                        });
+                    },
+                    (error) => {
+                        this.setState({
+                            isLoaded: true,
+                            error
+                        });
+                    }
+                )
+        }
+
+        render() {
+                const { error, isLoaded, items } = this.state;
+
+                if (error) {
+                    return <div > Error: { error.message } < /div>;
+                } else if (!isLoaded) {
+                    return <div > Loading... < /div>;
+                } else {
+
+                    return ( <
+                        Router >
+
+                        { /* component of FormsTitle for showing titles's link */ } <
+                        Route exact path = { `/` }
+                        component = {
+                            () => < FormsTitle i = { items }
+                            /> }/ >
+
+                            { /* component of FormDetails for getting details of form from backend */ } {
+                                items.map((item, index) => ( <
+                                        Route path = { `/user${index + 1}` }
+                                        component = {
+                                            () => < FormDetails idform = { item.id }
+                                            />}/ >
+                                        ))
+                                }
+
+                                <
+                                /Router>
+                            );
+                        }
+                    }
+                }
